de1e54bde775825024a542c3b675eaf9f962cf05
nn-TTT <aprv777@yahoo.co.jp>
2025-09-23 17:08:35 +0900

    トップページ

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,12 @@ export default function Home() {
       <div className="page_wrap">
         <div className="page_top">
           <div className="top_inner">
-            <h1 className="site_ttl txt_clr">
-              Portfolio<br />Page
-            </h1>
+          <h1 className="site_ttl txt_clr">
+            <TextSpan text="Portfolio" />
+            <br />
+            <TextSpan text="Page" />
+          </h1>
+
             <p className="page_content">
               作品一覧はこちら
               <span className="fn_s">※ID・PW入力あり</span>
